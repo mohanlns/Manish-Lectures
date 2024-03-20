d4decbd8c3e0949f638e29b786432bef1f36f36d
Ram mohan <mr.rm.manjhi@Rams-MacBook-Air-2.local>
2024-03-20 09:15:44 +0530

    lecture vise comited

---
+++ lecture_1/sumoflooped.js
@@ -0,0 +1,23 @@
+// Yaha p humlog sum of multiple times nikalenge.
+
+
+// all variables
+
+const variables = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6
+};
+
+// sum of 2 variables
+
+// sum of 3 variables
+
+// sum of 4 variables
+
+// sum of 5 variables
+
+// sum of 6 variables
--- lecture_1/variable.js
+++ lecture_1/variable.js
@@ -6,11 +6,9 @@
 // 2: var => These variables are reassignable
 // 3: const => const is not re assignable.
 
-
 let variable1 = 2;
 var variable2 = 3;
 const variable3 = 4;
-
 variable1 += 1; // is equal to variable1 = variable1 + 1;
 variable2 += 1;
 // variable3 += 1; // this part will throw an error of TypeError.
