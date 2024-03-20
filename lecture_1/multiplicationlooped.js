a258e381c2cc4ebeb2d86c1e88d8a08acd07764f
Ram mohan <mr.rm.manjhi@Rams-MacBook-Air-2.local>
2024-03-20 09:48:15 +0530

    multiplication code push ed

---
+++ lecture_1/multiplicationlooped.js
@@ -0,0 +1,22 @@
+// Yaha p humlog sum of multiple times nikalenge.
+
+
+// all variables
+//  Onject's are key value pairs.
+const variables = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6
+};
+
+// Types of loop?
+// for, foreach, filter, map
+
+let multiplications = 0
+
+// Type your code here.
+
+console.log("Total of all the values: ",multiplications);
