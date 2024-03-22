03d3423f6a6c66b808fd0041a9e726c4b37fcd3b
Ram mohan <mr.rm.manjhi@Rams-MacBook-Air-2.local>
2024-03-22 09:46:14 +0530

    switch condition

--- lecture_2/swith_condition_1.js
+++ lecture_2/swith_condition_1.js
@@ -9,3 +9,17 @@
 //   *
 // *****
 //   *
+
+
+const n = 5;
+for (var i = 1; i <= n; i++) {
+    const x = i%2;
+    switch (x){
+        case 0:
+            console.log(`${"*".repeat(5)}`);
+            break;
+        case 1:
+            console.log(`${" ".repeat(2)}${"*".repeat(1)}`);
+            break;
+    }
+}
