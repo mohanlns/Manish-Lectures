56fe76f28c3f4142e4a822633514a31af6f3f4f6
Ram mohan <mr.rm.manjhi@Rams-MacBook-Air-2.local>
2024-04-01 09:09:29 +0530

    prime number

--- lecture_3/functions.js
+++ lecture_3/functions.js
@@ -43,7 +43,37 @@ const startPatern_2 = async (n,i) => { // function with parameters
     console.log("Hello world!", "startPatern_2");
 }
 
+
+// 1*2 => 2
+// 1*2*3 => 6
+// 1*2*3*4 => 24
+// 1*2*3*4*5 => 120
+
+
+const factorials = async (currentValue,value) => { // function with parameters
+
+    for (let i = 1; i <= value; i++) {
+        currentValue = currentValue*i;
+    }
+
+    console.log("Factorial Value", currentValue);
+
+}
+
+
+// Check Prime Number: Check if a number is prime.
+// Jab bhi koi value prime hoti h to wo sirf apne aapse purn vibhajit joti h.
+
+const findPrime = async (value) => {
+    // Apply you logic here.
+}
+
+
+
 // how we can call function
 // startPatern_1_demo();
 // startPatern_1(5,0);
-startPatern_2(5,0)
+// startPatern_2(5,0);
+factorials(1,6);
+
+findPrime(2);
